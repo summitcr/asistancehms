4e00aafec651734be8f7fed46c0ef12d63e66198
jfarcode`s MacBook Pro <josealfacr@gmail.com>
2020-10-22 15:16:10 -0600

    add map routing

---
+++ src/app/map-routing/map-routing.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-routing',
+  templateUrl: './map-routing.page.html',
+  styleUrls: ['./map-routing.page.scss'],
+})
+export class MapRoutingPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
