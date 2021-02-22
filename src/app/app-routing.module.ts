94a1b39f1ccd3f0bf991153deb2d7dca7bf88c44
jairogarcia10 <jaigarcia12@gmail.com>
2021-02-22 14:16:13 -0600

    Mantener sesion iniciada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HeartrateComponent } from './heartrate/heartrate.component';
 import { CovidComponent } from './covid/covid.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'servicios', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
-  
+
   { path: 'see-people', loadChildren: './see-people/see-people.module#SeePeoplePageModule' },
   { path: 'heart-rate', component: HeartrateComponent },
   { path: 'modal-page', loadChildren: './modal-page/modal-page.module#ModalPagePageModule' },
@@ -15,10 +17,10 @@ const routes: Routes = [
   { path: 'Covid-19', component: CovidComponent },
   { path: 'coronavirus', loadChildren: './coronavirus/coronavirus.module#CoronavirusPageModule' },
   { path: 'confirmed', loadChildren: './confirmed/confirmed.module#ConfirmedPageModule' },
-  { path: 'registro-covid', loadChildren: './registro-covid/registro-covid.module#RegistroCovidPageModule' },
+  { path: 'registro-covid', loadChildren: './registro-covid/registro-covid.module#RegistroCovidPageModule', },
   { path: 'prenatal-control', loadChildren: './prenatal-control/prenatal-control.module#PrenatalControlPageModule' },
   { path: 'map-routing', loadChildren: './map-routing/map-routing.module#MapRoutingPageModule' },
-  { path: 'servicios', loadChildren: './servicios/servicios.module#ServiciosPageModule' },
+  { path: 'servicios', loadChildren: './servicios/servicios.module#ServiciosPageModule',canActivate:[AuthGuardService] },
   // { path: 'ticket', loadChildren: './ticket/ticket.module#TicketPageModule' },
   //{ path: 'maps/:id', loadChildren: './tab1/tab1.module#Tab1PageModule' },
 
---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,123 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { StorageService } from './storage.service';
+import { UtilStorageService } from './util-storage.service';
+
+const TOKEN_KEY = 'auth-token';
+
+export interface Token {
+  userCed: string;
+}
+export interface userModel {
+  identifierType: String,
+  identifier: String,
+  name: String,
+  age: String,
+  telephone: String,
+  address: String,
+
+}
+interface TokenResponse {
+  identifier: string;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  authenticationState = new BehaviorSubject(false);
+  private token: string;
+  userToken: any;
+  user: any;
+  constructor(private storage: Storage,
+    private plt: Platform,
+    private http: HttpClient,
+    private storeService: StorageService,
+    private localParam: UtilStorageService,) {
+    this.plt.ready().then(() => {
+      this.checkToken();
+    });
+  }
+  private saveToken(token: string): void {
+    this.storeService.localSave(this.localParam.localParam.mean, token);
+  }
+  checkToken() {
+    this.storeService.localGet(this.localParam.localParam.mean).then((resp) => {
+      this.user = resp;
+console.log(this.userToken);
+    }, (err) => {
+      console.error(err);
+    });
+  }
+  public getToken(): string {
+    if (!this.token) {
+      this.storeService.localGet(this.localParam.localParam.mean).then((resp) => {
+        this.token = resp;
+
+      }, (err) => {
+        console.error(err);
+      });
+
+    }
+    return this.token;
+  }
+  getTo() {
+    this.storeService.localGet(this.localParam.localParam.mean).then((resp) => {
+      this.userToken = resp;
+      console.log(this.userToken);
+    }, (err) => {
+      console.error(err);
+    });
+  }
+  // login() {
+  //   return this.storage.set(TOKEN_KEY, 'Bearer 1234567').then(() => {
+  //     this.authenticationState.next(true);
+  //   });
+  // }
+  private request(method: 'post' | 'get', url: string, user?: userModel | Token): Observable<any> {
+    let base;
+
+    if (method === 'post') {
+      base = this.http.post(url, user);
+    } else {
+      base = this.http.get(url, { headers: { Authorization: `Bearer ${this.getTo()}` } });
+    }
+
+    const request = base.pipe(
+      map((datas: TokenResponse) => {
+        if (datas.identifier) {
+          this.saveToken(datas.identifier);
+        }
+        return datas;
+      })
+    );
+
+    return request;
+  }
+  public login(user: Token, url: string): Observable<any> {
+    return this.request('get', url, user);
+  }
+  logout() {
+    return this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+
+  isAuthenticated() {
+    return this.authenticationState.value;
+  }
+  public isLoggedIn(): boolean {
+
+    const user = this.getToken();
+    console.log(user);
+    if (user) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+}//fin de class
